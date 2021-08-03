dcacf3e46f9c9b0d0cf3c5b0d2ca02e30d791600
Avadanei Ciprian <avadaneicipriancordun@gmail.com>
2021-08-04 00:58:17 +0300

    Done, almost all

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 function app() {
-    let bill, people, tip, totalPerPerson, tipPerPerson;
+    let bill, people, tip, totalPerPerson, tipPerPerson, totalText, tipText;
     bill = parseInt(document.querySelector("#bill").value);
 
     people = parseInt(document.querySelector("#people").value);
 
     tip = parseInt(document.querySelector("input[name=tip]:checked").value);
 
+    totalText = document.querySelector('#totalText');
+    tipText = document.querySelector('#tipText');
+
     if (document.querySelector("#tip6").checked === true) {
         tip = parseInt(customButton.value);
     }
 
-
-    console.log("Tip", tip);
-    console.log("Bill", bill);
-    console.log("People", people);
-
     totalPerPerson = (bill + bill * tip / 100) / people;
     tipPerPerson = bill * tip / 100 / people
 
-    console.log("Total", totalPerPerson);
-    console.log("tipPerPerson", tipPerPerson);
+    totalText.innerHTML = totalPerPerson;
+    tipText.innerHTML = tipPerPerson;
 }
 
 let customButton = document.querySelector("#custom");
 customButton.onclick = () => {
     document.querySelector("#tip6").checked = true;
     app();
+}
+
+let resetButton = document.querySelector("#button");
+resetButton.onclick = () => {
+    document.querySelector("form").reset();
 }
